bd5f2b07907b8a94ca669d722c475e56464a2065
Daniel Maus <hakumennomodo@gmail.com>
2025-04-08 21:51:00 -0300

    Criação da tela principal

---
+++ SolucaoAlgoritmo6/Algoritmo6/Form1.cs
@@ -0,0 +1,40 @@
+namespace Algoritmo6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
